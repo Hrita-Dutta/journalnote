9ba54e77391e1b1f18b066ab386e54cac6d4028a
Hrita Dutta <hritadutta@gmail.com>
2026-01-24 13:21:08 +0545

    added streaks in navbar for navigation

--- JournalNote/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/JournalNote.AssemblyInfo.cs
+++ JournalNote/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/JournalNote.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5c816e105b367889777d1dc47442539f2bf5ab72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+af984f1a4d0d9459563cc5a054d1e4845db0b0d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
