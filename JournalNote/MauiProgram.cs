9ca5f5699ea0262f0ddbbaf65c8f12dc9f03c0b1
Hrita Dutta <hritadutta@gmail.com>
2026-01-26 00:33:27 +0545

    created pdf export service and updated mauiprogram.cs

--- JournalNote/MauiProgram.cs
+++ JournalNote/MauiProgram.cs
@@ -18,7 +18,7 @@ public static class MauiProgram
         builder.Services.AddSingleton<DatabaseService>();
         builder.Services.AddSingleton<ThemeService>();
         builder.Services.AddSingleton<SecurityService>();
-
+        builder.Services.AddSingleton<PdfExportService>();
 
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
