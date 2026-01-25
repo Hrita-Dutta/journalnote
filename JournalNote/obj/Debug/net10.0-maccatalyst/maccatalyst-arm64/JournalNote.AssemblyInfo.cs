e69416c37a72fc3514a0edbe52b6037debc0127d
Hrita Dutta <hritadutta@gmail.com>
2026-01-25 17:30:21 +0545

    added themes page with loading state

--- JournalNote/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/JournalNote.AssemblyInfo.cs
+++ JournalNote/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/JournalNote.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+174b3af154210762e27112cd9bc998d178826aa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4f2c1784f0e860ab5ebcc3f61b3a67cb34e8f231")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
