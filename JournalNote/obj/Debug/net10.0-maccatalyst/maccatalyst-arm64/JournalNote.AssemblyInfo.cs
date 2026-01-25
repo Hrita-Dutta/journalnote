2d487eb5a4ba7d0adc4164fafa664726336cab96
Hrita Dutta <hritadutta@gmail.com>
2026-01-25 19:05:17 +0545

    refactor page and reorganize page logic for themes and analytics

--- JournalNote/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/JournalNote.AssemblyInfo.cs
+++ JournalNote/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/JournalNote.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5f2def2253d069652f5dbbc87d26dc660f3b7ef9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9b630b5ff675c9a669edbb55d012421e9eb627f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
